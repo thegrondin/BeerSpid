f93ac3edc41aaef68a221860c683b89c525220a7
1731481 <tomtom999@hotmail.ca>
2019-09-29 22:11:28 -0400

    Adding Routing support in Boostrapper

--- Libs/BeerSpid/Boostrapper/Bootstrap.php
+++ Libs/BeerSpid/Boostrapper/Bootstrap.php
@@ -2,22 +2,48 @@
 
 class Boostrap implements IBoostrap {
 
+    private $container;
+    private $router;
+    private $ressourcesRegistered;
+
+    function __construct() {
+        $this->container = new DIContainer();
+        $this->router = $this->container->getInstance(IRouter::class);
+        $this->ressourcesRegistered = false;
+    }
+
     public function initialize() {
+
+    }
+
+    public function initializeRoutes() {
+
+        if (!$this->ressourcesRegistered) {
+            return;
+        }
+
+        $routeCollection = $this->container->getInstance(IRouteCollection::class);
+
+        $routes = json_decode(Config::get('routes.json'), true);
+        foreach ($routes as $route) {
+            $routeCollection->add($route);
+        }
+        $this->router->addRoutes($routeCollection);
 
     }
 
     public function registerRessources(array $additionalRessources) {
 
-        $container = new DIContainer();
-
         $ressources = array_merge(json_decode(Config::get("dependencies.json"), true), $additionalRessources);
 
         foreach ($ressources as $dependency) {
-            $container->register(/*Reflexion class with parameters*/);
+            $this->container->register((DIRessource::default($dependency['interface'], $dependency['class'], $dependency['parameters']));
         }
+
+        $this->ressourcesRegistered = true;
     }
 
     public function start() {
-
+        $this->router->dispatch($_SERVER["REQUEST_URI"]);
     }
 }
